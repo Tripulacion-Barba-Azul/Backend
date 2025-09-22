47f6eb785e32765d660d54d9067802a4d116e903
Eliseo Facchin <eliseofacchin@mi.unc.edu.ar>
2025-09-22 17:51:38 +0000

    ING-32 add SQLAlchemy db configuration

--- src/settings.py
+++ src/settings.py
@@ -1,5 +1,14 @@
 """Defines project settings"""
 
+from pydantic_settings import BaseSettings
+
+
+class Settings(BaseSettings):
+    """Project settings definition"""
+
+    ROOT_PATH: str = ""
+    DB_FILEANAME: str = "sqlite:///deathonthecards.db"
 
 
 
+settings = Settings()
