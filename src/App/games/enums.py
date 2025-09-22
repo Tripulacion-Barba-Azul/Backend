5c43fe83af88e9f9e4cb0127081e398943e26618
Eliseo Facchin <eliseofacchin@mi.unc.edu.ar>
2025-09-22 22:04:47 +0000

    Revert "feature_ING-32_Modelos_jugador_y_partida"
    This reverts commit 8943680d6adb1039982aa4b5a1a5e3416c32b68f, reversing
    changes made to 1921bd5249daaca8f648db73d606d54fbf505f14.

--- src/App/games/enums.py
+++
@@ -1,6 +0,0 @@
-from enum import Enum
-
-class GameStatus(Enum):
-    WAITING = "waiting"
-    IN_PROGRESS = "in_progress"
-    FINISHED = "finished"
--- src/settings.py
+++ src/settings.py
@@ -1,14 +1,5 @@
 """Defines project settings"""
 
-from pydantic_settings import BaseSettings
-
-
-class Settings(BaseSettings):
-    """Project settings definition"""
-
-    ROOT_PATH: str = ""
-    DB_FILEANAME: str = "sqlite:///deathonthecards.db"
 
 
 
-settings = Settings()
