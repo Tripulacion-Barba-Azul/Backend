ef8ae16bad9964fc835aba249e24d2363fdb23e4
Santiago <santiagozuk@mi.unc.edu.ar>
2025-09-24 12:59:32 -0300

    add class WebsocketManage

---
+++ src/App/websockets.py
@@ -0,0 +1,38 @@
+"""Service websocket"""
+
+from fastapi import APIRouter, WebSocket, WebSocketDisconnect
+
+websocket_router = APIRouter()
+
+class WebsocketManage:
+    def __init__(self):
+        self.active_connections: list[WebSocket] = []
+
+    async def connect(self, websocket: WebSocket):
+        await websocket.accept()
+        self.active_connections.append(websocket)
+
+    async def close_connection(self, websocket: WebSocket):
+        if websocket in self.active_connections:
+            await websocket.close()
+            self.active_connections.remove(websocket)
+
+
+    async def send_message(self, message: str, websocket: WebSocket):
+        await websocket.send_text(message)
+
+    async def broadcast(self, message: str):
+        for connection in self.active_connections[:]:
+            await connection.send_text(message)
+
+    async def broadcast_except(self, message: str, skip_ws: WebSocket):
+        for connection in self.active_connections:
+            if connection != skip_ws:
+                try:
+                    await connection.send_text(message)
+                except Exception:
+                    self.active_connections.remove(connection)
+
+    def count(self) -> int:
+        return len(self.active_connections)
+
