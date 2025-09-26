dd9ca35077cea914f2cfea2ee215f7f2917e93eb
Santiago <santiagozuk@mi.unc.edu.ar>
2025-09-26 20:27:59 -0300

    add exceptions

---
+++ src/App/exceptions.py
@@ -0,0 +1,15 @@
+"""Exceptions"""
+
+class GameNotFoundError(Exception):
+    """Se lanza cuando no se encuentra un juego con el id especificado."""
+    pass
+
+
+class GameFullError(Exception):
+    """Se lanza cuando un juego ya alcanzó el número máximo de jugadores."""
+    pass
+
+
+class GameAlreadyStartedError(Exception):
+    """Se lanza cuando se intenta unir a un juego que ya ha comenzado."""
+    pass
