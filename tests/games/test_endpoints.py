e209b601b822b96a5fb29971f6ac5c3d094e0cb0
Eliseo Facchin <eliseofacchin@mi.unc.edu.ar>
2025-09-27 21:35:16 +0000

    ING-34 add test get_game_by_id

--- tests/games/test_endpoints.py
+++ tests/games/test_endpoints.py
@@ -2,6 +2,7 @@ from datetime import date
 from fastapi.testclient import TestClient
 
 
+
 def test_get_games(client: TestClient, seed_games):
     
     response = client.get("/games")
@@ -17,6 +18,24 @@ def test_get_games(client: TestClient, seed_games):
 
     assert game1Rs["gameId"] == game1.id
     assert game1Rs["ownerName"] == game1.owner.name
+
+def test_get_game_by_id_succes(client: TestClient, seed_games):
+    response = client.get("/games/1")
+    data = response.json()
+    game = seed_games["games"][0]
+    
+    assert response.status_code == 200
+
+    assert data["gameId"] == game.id
+    assert data["gameName"] == game.name
+    assert data["minPlayers"] == game.min_players
+    assert data["maxPlayers"] == game.max_players
+    assert data["ownerId"] == game.owner_id
+    
+    assert len(data["players"]) == 1
+    player_re = data["players"][0]
+    assert player_re["playerName"] == game.players[0].name
+    assert player_re["birthDate"] == game.players[0].birthday.strftime('%Y-%m-%d')
 
 
 def test_create_game_success(client: TestClient):
