841ea606cd6462c3a5530ea8c05b7be2a0d06577
Santiago <santiagozuk@mi.unc.edu.ar>
2025-09-27 18:12:06 -0300

    add test game full

--- tests/games/test_endpoints.py
+++ tests/games/test_endpoints.py
@@ -114,3 +114,62 @@ def test_join_game_not_found(client: TestClient):
 
     assert response.status_code == 404
     assert data["detail"] == "El juego con id 9999 no existe."
+
+def test_join_game_full(client: TestClient):
+
+        # Create a game to join
+    player_info = {
+                "playerName":"Barba Azul",
+                "birthDate":date(2000,1,1).strftime("%Y-%m-%d")
+    }
+
+    game_info = {
+                "gameName":"Tripulación de Barba Azul",
+                "minPlayers":2,
+                "maxPlayers":2
+    }
+    
+    response = client.post(
+        "/games", 
+        json = {
+            "player_info": player_info,
+            "game_info": game_info,
+        },
+    )
+
+    data = response.json()
+    game_id = data["gameId"]
+    with client.websocket_connect(f"/ws/{game_id}") as websocket:
+        result = websocket.receive_json()
+        assert result == {"msj" : f"Connected to ws from game {game_id}"}
+
+        # Join the created game
+        new_player_info = {
+                    "playerName":"Capitan",
+                    "birthDate":date(2001,4,5).strftime("%Y-%m-%d")
+        }
+
+        response = client.post(
+            f"/games/{game_id}/join", 
+            json = new_player_info,
+        )
+
+        data = response.json()
+
+        assert response.status_code == 200
+
+        # Try to join the created game with a third player
+        second_player_info = {
+                    "playerName":"Oficial",
+                    "birthDate":date(1999,12,31).strftime("%Y-%m-%d")
+        }
+
+        response = client.post(
+            f"/games/{game_id}/join", 
+            json = second_player_info,
+        )
+
+        data = response.json()
+
+        assert response.status_code == 400
+        assert data["detail"] == "El juego ya ha alcanzado el número máximo de jugadores."
