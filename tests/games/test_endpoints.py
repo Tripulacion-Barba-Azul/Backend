9de98a9ade49a8d9daf0bd5690e230a598dc753c
Santiago <santiagozuk@mi.unc.edu.ar>
2025-09-27 17:52:06 -0300

    add test

--- tests/games/test_endpoints.py
+++ tests/games/test_endpoints.py
@@ -53,3 +53,47 @@ def test_create_game_invalid_data(client: TestClient):
     assert response.status_code == 400
     assert response.json()["detail"] == "El máximo de jugadores debe ser mayor o igual al mínimo."
     
+def test_join_game_success(client: TestClient):
+
+    # Create a game to join
+    player_info = {
+                "playerName":"Barba Azul",
+                "birthDate":date(2000,1,1).strftime("%Y-%m-%d")
+    }
+
+    game_info = {
+                "gameName":"Tripulación de Barba Azul",
+                "minPlayers":2,
+                "maxPlayers":4
+    }
+    
+    response = client.post(
+        "/games", 
+        json = {
+            "player_info": player_info,
+            "game_info": game_info,
+        },
+    )
+
+    data = response.json()
+    game_id = data["gameId"]
+    with client.websocket_connect(f"/ws/{game_id}") as websocket:
+        result = websocket.receive_json()
+        assert result == {"msj" : f"Connected to ws from game {game_id}"}
+
+        # Join the created game
+        new_player_info = {
+                    "playerName":"Capitan",
+                    "birthDate":date(2001,4,5).strftime("%Y-%m-%d")
+        }
+
+        response = client.post(
+            f"/games/{game_id}/join", 
+            json = new_player_info,
+        )
+
+        data = response.json()
+
+        assert response.status_code == 200
+        assert data["gameId"] == game_id
+        assert data["actualPlayerId"] is not None
