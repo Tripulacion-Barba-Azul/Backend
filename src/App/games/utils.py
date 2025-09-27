837b803661fc910a11eaf80eb119c9e818779c46
Santiago <santiagozuk@mi.unc.edu.ar>
2025-09-27 13:04:08 -0300

    add info player in utils

--- src/App/games/utils.py
+++ src/App/games/utils.py
@@ -1,9 +1,16 @@
 from App.games.models import Game
-from App.games.schemas import GameInfo
+from App.games.schemas import GameInfo, GameInfoPlayer
+from App.players.models import Player
 
 
 def db_game_2_game_info(db_game: Game) -> GameInfo:
     return GameInfo(
         gameId=db_game.id,
         ownerId=db_game.owner_id,
+    )
+
+def db_game_2_game_info_player(db_game: Game, db_player: Player) -> GameInfoPlayer:
+    return GameInfoPlayer(
+        gameId=db_game.id,
+        actualPlayerId=db_player.id,
     )
