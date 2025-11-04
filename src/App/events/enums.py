6063a0ece558ac6c42c0579dcf96cc898ad62897
Eliseo Facchin <eliseofacchin@mi.unc.edu.ar>
2025-11-04 19:45:25 +0000

    ING-167 add resolve method for computing result of stack of unresolved events

--- src/App/events/enums.py
+++ src/App/events/enums.py
@@ -1,10 +1,10 @@
 from enum import Enum  
 
 class EventType(Enum):
-    PLAY_SET = "play_set"
-    PLAY_CARD = "play_card"
-    ADD_DETECTIVE_TO_SET = "add_detective_to_set"
-    PLAY_NSF = "play_NSF"
-    RECEIVE_DEVIOUS = "receive_devious"
-    DISCARD_ETTP = "discard_ETTP"
-    POINT_YOUR_SUSPICIONS = "point_your_suspicions"
+    PLAY_SET = "play set"
+    PLAY_CARD = "play card"
+    PLAY_DETECTIVE = "play detective"
+    PLAY_NSF = "play NSF"
+    RECEIVE_DEVIOUS = "receive devious"
+    DISCARD_ETTP = "discard ETTP"
+    POINT_YOUR_SUSPICIONS = "point your suspicions"
