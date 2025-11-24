ad61ef8b96770177f62d5319976531321cb8c42d
Marco Scavuzzo <114264461+MarcoSca35@users.noreply.github.com>
2025-11-24 03:25:25 -0300

    Refactor import paths in play_detective_resolver.py

--- src/App/events/resolvers/play_detective_resolver.py
+++ src/App/events/resolvers/play_detective_resolver.py
@@ -1,6 +1,6 @@
-from App.events.resolvers.base_resolver import BaseEventResolver
-from App.players.enums import TurnAction, TurnStatus
-from App.sets.services import DetectiveSetService
+from src.App.events.resolvers.base_resolver import BaseEventResolver
+from src.App.players.enums import TurnAction, TurnStatus
+from src.App.sets.services import DetectiveSetService
 
 
 class PlayDetectiveResolver(BaseEventResolver):
@@ -42,3 +42,4 @@ class PlayDetectiveResolver(BaseEventResolver):
         self._db.commit()
 
         return turn_action
+
