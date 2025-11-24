a2ae8cac4c4b3a330fd141c0c91019f89bca11a3
Marco Scavuzzo <114264461+MarcoSca35@users.noreply.github.com>
2025-11-24 03:17:27 -0300

    Fix import paths in utils.py

--- src/App/sets/utils.py
+++ src/App/sets/utils.py
@@ -1,6 +1,6 @@
-from App.sets.models import DetectiveSet
-from App.sets.schemas import SetPublicInfo
-from App.card.utils import db_card_2_card_info
+from src.App.sets.models import DetectiveSet
+from src.App.sets.schemas import SetPublicInfo
+from src.App.card.utils import db_card_2_card_info
 
 
 def db_dset_2_set_public_info(db_dset: DetectiveSet) -> SetPublicInfo:
@@ -9,4 +9,5 @@ def db_dset_2_set_public_info(db_dset: DetectiveSet) -> SetPublicInfo:
         setName=db_dset.type.value,
         cards = [db_card_2_card_info(card) for card in db_dset.cards]
         
+
     )
